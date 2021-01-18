25b33ed08d3ebb1c839094603791a9cebc1cbebc
Ivan Mincione <ivan.mincione92@outlook.it>
2021-01-18 14:15:36 +0100

    Init class User

---
+++ User.php
@@ -0,0 +1,13 @@
+<?php
+
+    class User {
+        public $user_id;
+        public $first_name;
+        public $last_name;
+        public $age;
+        public $email;
+        public $nationality;
+
+    }
+
+ ?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+    // definire la struttura di una tabella users che contiene i dati degli utenti di un blog
+    // definire una classe User che "mappa" la tabella users
+    // creare alcune istanze della classe User per stampare i dati degli utenti
+
+    require "User.php";
+
+
+ ?>
