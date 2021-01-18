1c825d6f0c3b5a05d5eb6380d90f873fe9e8feac
Ivan Mincione <ivan.mincione92@outlook.it>
2021-01-18 14:53:39 +0100

    Add and print users

--- User.php
+++ User.php
@@ -8,6 +8,27 @@
         public $email;
         public $nationality;
 
+        public function __construct ($_first_name, $_last_name, $_age) {
+           $this->first_name = $_first_name;
+           $this->last_name = $_last_name;
+           $this->age = $_age;
+        }
+
+
+        public function getName() {
+            return $this->first_name;
+        }
+
+        public function getLastName() {
+            return $this->last_name;
+        }
+
+        public function getAge() {
+            return $this->age;
+        }
+
     }
+
+
 
  ?>
--- index.php
+++ index.php
@@ -6,5 +6,20 @@
 
     require "User.php";
 
+    $user1 = new User("Luca", "Neri", "20");
+    echo "<h1> NOME : " . $user1->getName() . "</h1>";
+    echo "<h1> COGNOME : " . $user1->getLastName() . "</h1>";
+    echo "<h1> ETA' : " . $user1->getAge() . "</h1>" . "<br>";
+
+    $user2 = new User("Paolo", "Rossi", "15");
+    echo "<h1> NOME : " . $user2->getName() . "</h1>";
+    echo "<h1> COGNOME : " . $user2->getLastName() . "</h1>";
+    echo "<h1> ETA' : " . $user2->getAge() . "</h1>" . "<br>";
+
+    $user3 = new User("Mario", "Bianchi", "25");
+    echo "<h1> NOME : " . $user3->getName() . "</h1>";
+    echo "<h1> COGNOME : " . $user3->getLastName() . "</h1>";
+    echo "<h1> ETA' : " . $user3->getAge() . "</h1>" . "<br>";
+
 
  ?>
